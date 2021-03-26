c4698839a9fb6a6eeba9f2b5d75f47f9997d6540
Maksim Rakitin <mrakitin@bnl.gov>
2021-03-26 17:52:14 -0400

    Remove run_test.py and use the test block

--- recipe/run_test.py
+++
@@ -1,8 +0,0 @@
-import gi
-gi.require_version("GLib", "2.0")
-gi.require_version("Gtk", "3.0")
-assert("__init__" in gi.__file__)
-from gi.repository import GLib, Gtk
-assert(31 == GLib.Date.get_days_in_month(GLib.DateMonth.JANUARY, 2000))
-assert(3 == Gtk.get_major_version())
-
